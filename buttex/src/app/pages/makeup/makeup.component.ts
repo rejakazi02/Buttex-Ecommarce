6aeb13f425918b0b70525ff2d0c6bfe9706764de
Reja Kazi <rejakazi02@gmail.com>
2022-07-05 05:21:10 +0600

    makeup

--- buttex/src/app/pages/makeup/makeup.component.ts
+++ buttex/src/app/pages/makeup/makeup.component.ts
@@ -12,4 +12,100 @@ export class MakeupComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  fraProducts:any[] =[
+    {
+      _id:1,
+      title:"Kurti",
+      subTitle:"Shop Now",
+      routerLink:'#',
+      desc:'GREY POLYESTER VISCOSE KAMEEZ SHALWAR | JJKS-A-44018',
+      price:' 4500',
+      priceDis:' 4500',
+      image:'../../../assets/images/products/makeup/p1.webp',
+
+
+    },
+    {
+      _id:2,
+      title:"Stich",
+      subTitle:"Shop Now",
+      routerLink:'#',
+      desc:'WHITE BLENDED KAMEEZ SHALWAR | JCKS-45009',
+      price:'bdt 4500',
+      priceDis:'bdt 4500',
+      image:"../../../assets/images/products/makeup/p2.webp"
+    },
+    {
+      _id:3,
+      title:"unstich",
+      subTitle:"Shop Now",
+      routerLink:'#',
+      desc:'GREY BLENDED KURTA | JCK-43129',
+      price:'bdt 4500',
+      priceDis:'bdt 4500',
+      image:"../../../assets/images/products/makeup/p3.webp"
+    },
+    {
+      _id:4,
+      title:"unstich",
+      subTitle:"Shop Now",
+      routerLink:'#',
+      desc:'CREAM POLYESTER VISCOSE KAMEEZ SHALWAR | JCKS-43126',
+      price:'bdt 4500',
+      priceDis:'bdt 4500',
+      image:"../../../assets/images/products/makeup/p4.webp"
+    },
+    {
+      _id:5,
+      title:"unstich",
+      subTitle:"Shop Now",
+      routerLink:'#',
+      desc:'GREY BLENDED KURTA | JCK-43129',
+      price:'bdt 4500',
+      priceDis:'bdt 4500',
+      image:"../../../assets/images/products/makeup/p9.webp"
+    },
+    {
+      _id:6,
+      title:"unstich",
+      subTitle:"Shop Now",
+      routerLink:'#',
+      desc:'GREY POLYESTER VISCOSE KAMEEZ SHALWAR | JJKS-A-44018',
+      price:'bdt 4500',
+      priceDis:'bdt 4500',
+      image:"../../../assets/images/products/makeup/p5.webp"
+    },
+    {
+      _id:7,
+      title:"unstich",
+      subTitle:"Shop Now",
+      routerLink:'#',
+      desc:'GREY POLYESTER VISCOSE KAMEEZ SHALWAR | JJKS-A-44018',
+      price:'bdt 4500',
+      priceDis:'bdt 4500',
+      image:"../../../assets/images/products/makeup/p6.webp"
+    },
+    {
+      _id:8,
+      title:"unstich",
+      subTitle:"Shop Now",
+      routerLink:'#',
+      desc:'CREAM POLYESTER VISCOSE KAMEEZ SHALWAR | JCKS-43126',
+      price:'bdt 4500',
+      priceDis:'bdt 4500',
+      image:"../../../assets/images/products/makeup/p7.webp"
+    },
+    {
+      _id:9,
+      title:"unstich",
+      subTitle:"Shop Now",
+      routerLink:'#',
+      desc:'CREAM POLYESTER VISCOSE KAMEEZ SHALWAR | JCKS-43126',
+      price:'bdt 4500',
+      priceDis:'bdt 4500',
+      image:"../../../assets/images/products/makeup/p8.webp"
+    }
+  ]
+
 }
